547b13e0ef7569f47e7ce2fd6ff25078eb10a5fb
hugomalopes <huguinhoman@gmail.com>
2014-08-07 02:57:40 +0100

    QR AddClothing module added

--- android/dressitup/src/com/app/dressitup/MenuActivity.java
+++ android/dressitup/src/com/app/dressitup/MenuActivity.java
@@ -1,13 +1,29 @@
 package com.app.dressitup;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MenuActivity extends Activity {
+	
+	private Button addClothingButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_menu);
+		
+		addClothingButton = (Button)findViewById(R.id.add_clothing_btn);
+		addClothingButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MenuActivity.this, AddClothingActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 }
